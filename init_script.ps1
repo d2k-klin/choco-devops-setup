ed882d766f0a44163f421262d2da5ca665a56edb
Davor <davorklin@live.com>
2021-04-15 23:02:15 +0200

    Update + add additional tools

--- init_script.ps1
+++ init_script.ps1
@@ -3,36 +3,63 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 
 choco upgrade chocolatey -y
 choco install chocolatey-core.extension -y
-######### Install choco packages
-choco install notepadplusplus googlechrome atom 7zip keepass -y
+######### base
+choco install notepadplusplus googlechrome firefox atom 7zip keepass -y
 choco install vscode -y
-choco install wsl2 -y
-choco install git.install -y
-choco install putty.install -y
-choco install awscli -y
-choco install terraform -y
-choco install terragrunt -y
-choco install docker-desktop -y
-choco install kubernetes-cli -y
-#choco install minikube -y
-choco install screenpresso -y
-choco install adobereader -y
-choco install firefox -y
-###optional
+choco install vscode-kubernetes-tools vscode-python vscode-java vscode-powershell vscode-yaml -y
+choco install cmder -y
+choco install wsl2 --version=1.0.0 -y
+choco install git -y
+choco install tortoisegit -y
+choco install putty -y
 choco install curl -y
 choco install jdk11 -y
-choco install awstools.powershell -y
-choco install openvpn -y
-choco install openssh -y
+choco install nodejs-lts -y
+choco install make -y
 choco install python -y
-choco install filezilla -y
-choco install putty -y
-choco install winscp -y
+choco install pip -y
+
+# AWS + terraform
+choco install awscli -y
+choco install awstools.powershell -y
+choco install terraform -y
+choco install terragrunt -y
+
+#Docker + k8s
+choco install docker-desktop -y
+choco install kubernetes-cli -y
+choco install kubernetes-helm -y
+choco install kubectx -y
+choco install lens -y
+choco install kubens -y
+choco install minikube -y
+
+#windows tools
+choco install screenpresso -y
+choco install adobereader -y
 choco install irfanview -y
 choco install microsoft-teams -y
 choco install slack -y
-choco install tortoisegit -y
+choco install zoom -y
+choco install spotify -y
 
+# sysadmin
+choco install openvpn -y
+choco install openssh -y
+choco install filezilla -y
+choco install putty -y
+choco install winscp -y
+choco install cygwin -y
+
+####pip pckages
+#https://awsu.me/general/overview.html
+pip install awsume
+
+#https://www.checkov.io/1.Welcome/What%20is%20Checkov.html
+pip install checkov
+
+#https://cloudsplaining.readthedocs.io/en/latest/
+pip3 install --user cloudsplaining
 
 ####add github key
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
---
+++ update_script.ps1
@@ -0,0 +1,49 @@
+choco upgrade chocolatey -y
+choco upgrade chocolatey-core.extension -y
+######### base
+choco upgrade notepadplusplus googlechrome firefox atom 7zip keepass -y
+choco upgrade vscode -y
+choco upgrade vscode-kubernetes-tools vscode-python vscode-java vscode-powershell vscode-yaml -y
+choco upgrade cmder -y
+choco upgrade wsl2 --version=1.0.0 -y
+choco upgrade git -y
+choco upgrade tortoisegit -y
+choco upgrade putty -y
+choco upgrade curl -y
+choco upgrade jdk11 -y
+choco upgrade nodejs-lts -y
+choco upgrade make -y
+choco upgrade python -y
+choco upgrade pip -y
+
+# AWS + terraform
+choco upgrade awscli -y
+choco upgrade awstools.powershell -y
+choco upgrade terraform -y
+choco upgrade terragrunt -y
+
+#Docker + k8s
+choco upgrade docker-desktop -y
+choco upgrade kubernetes-cli -y
+choco upgrade kubernetes-helm -y
+choco upgrade kubectx -y
+choco upgrade lens -y
+choco upgrade kubens -y
+choco upgrade minikube -y
+
+#windows tools
+choco upgrade screenpresso -y
+choco upgrade adobereader -y
+choco upgrade irfanview -y
+choco upgrade microsoft-teams -y
+choco upgrade slack -y
+choco upgrade zoom -y
+choco upgrade spotify -y
+
+# sysadmin
+choco upgrade openvpn -y
+choco upgrade openssh -y
+choco upgrade filezilla -y
+choco upgrade putty -y
+choco upgrade winscp -y
+choco upgrade cygwin -y
